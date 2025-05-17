8dc8d434796808149f3cc85bfdd3ca70351fba30
Elmano Neto <elmanoneto@gmail.com>
2025-05-17 12:48:05 -0300

    test

--- app/api/v1/routes.py
+++ app/api/v1/routes.py
@@ -3,6 +3,4 @@ from fastapi import APIRouter
 from app.api.v1.endpoints import sentiment
 
 router = APIRouter()
-router.include_router(
-    sentiment.router, prefix="/sentiment", tags=["Sentiment Analysis"]
-)
+router.include_router(sentiment.router, prefix="/sentiment", tags=["SentimentAnalysis"])
